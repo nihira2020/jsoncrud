49e306822fddfb77ccebf499002582eb4a0b5fd1
Natarajan <nataraj.itsolution@gmail.com>
2022-09-05 15:08:42 +0530

    Menu updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyComponent } from './company/company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:CompanyComponent,path:"company"
+  },
+  {
+    component:CompanyComponent,path:""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
